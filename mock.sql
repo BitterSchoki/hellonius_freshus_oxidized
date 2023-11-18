16cefbb8cb6e9b4f66d3869aad9ccb9d34b81564
blackSwan566 <57305281+blackSwan566@users.noreply.github.com>
2023-11-18 19:46:38 +0100

    Food_groups

--- mock.sql
+++ mock.sql
@@ -22,19 +22,19 @@ insert into ingredients values (14, 'Basil', '');
 --Ingredients mushroom risotto vegan
 insert into ingredients values (15, 'Vegan white wine', '');
 insert into ingredients values (16, 'Vegan butter', '');
-insert into ingredients values (17, 'Vegan Pramesan', '');
+insert into ingredients values (17, 'Vegan Parmesan', '');
 
 -- More ingredients
 insert into ingredients values (18, 'Milk', '');
 insert into ingredients values (19, 'Egg', '');
 insert into ingredients values (20, 'Shrimps', '');
 insert into ingredients values (21, 'Soy', '');
+insert into ingredients values (22, 'Mehl', '');
 
 -- AMOUNT AND ADD INGREDIENTS TO RECIPE
--- Uniqueid, receipe id, ingridient id, anzahl,einheit 
+-- Unique, receipe id, ingridient id, amount, unit  
 --Mushroom Risotto
 insert into recipe_ingredients values (1, 1, 3, 500, 'ml');
-
 insert into recipe_ingredients values (2, 2, 1, 200, 'g');
 insert into recipe_ingredients values (3, 2, 2, 1.5, 'pcs');
 
@@ -45,5 +45,13 @@ insert into ingredient_food_groups values(2, 5, 3);
 insert into ingredient_food_groups values(3, 6, 3);
 insert into ingredient_food_groups values(4, 8, 3);
 insert into ingredient_food_groups values(5, 11, 3);
-insert into ingredient_food_groups values(5, 11, 1);
-insert into ingredient_food_groups values(6, 12, 1);
+insert into ingredient_food_groups values(6, 11, 1);
+insert into ingredient_food_groups values(7, 12, 1);
+insert into ingredient_food_groups values(8, 15, 3);
+insert into ingredient_food_groups values(11, 18, 1);
+insert into ingredient_food_groups values(12, 19, 1);
+insert into ingredient_food_groups values(13, 20, 4);
+insert into ingredient_food_groups values(13, 21, 3);
+insert into ingredient_food_groups values(14, 22, 1);
+
+
