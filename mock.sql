a340c44c461a92711d904701ebb919a605e88231
Florian Lercher <florian.lercher@tum.de>
2023-11-18 11:45:15 +0100

    Add mock data

---
+++ mock.sql
@@ -0,0 +1,10 @@
+insert into recipes values (1, 'Curry', 'A nice, hearty curry', 2);
+insert into recipes values (2, 'Salad', 'A fresh, green salad', 4);
+
+insert into ingredients values (1, 'Lettuce', 'Just your plain old lettuce');
+insert into ingredients values (2, 'Cucumber', 'Long and green');
+insert into ingredients values (3, 'Coconut Milk', 'Creamy!');
+
+insert into recipe_ingredients values (1, 1, 3, 500, 'ml');
+insert into recipe_ingredients values (2, 2, 1, 200, 'g');
+insert into recipe_ingredients values (3, 2, 2, 1.5, 'pcs');
