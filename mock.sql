f4a61210e1b41e4269a8b55336f2fc7f15c27540
blackSwan566 <57305281+blackSwan566@users.noreply.github.com>
2023-11-18 23:44:15 +0100

    Merge

--- mock.sql
+++ mock.sql
@@ -4,7 +4,7 @@ insert into recipes values (2, 'Dumplings', 'A Bavarian receipe', 2);
 insert into recipes values (3, 'Leberkaese Burger', 'A Bavarian receipe', 4);
 
 --INGREDIENTS
---Ingridients Mushroom Risotto
+--Ingredients Mushroom Risotto
 insert into ingredients values (1, 'Mushrooms', 'Stone','mushrooms');
 insert into ingredients values (2, 'Mushrooms', 'Normal', 'mushrooms');
 insert into ingredients values (3, 'Water', 'Tap','x');
@@ -46,24 +46,58 @@ insert into ingredients values (30, 'Coffee', '', 'coffee');
 insert into ingredients values (31, 'Peanuts', '', 'nuts');
 insert into ingredients values (32, 'Tree nuts', '', 'nuts');
 
-
 --Leberkaese
-insert into ingredients values (33, 'Senf', '', 'senf'); -- clean
-insert into ingredients values (34, 'Coleslaw', '', 'salat'); --lc
-insert into ingredients values (35, 'Radishes', '', 'radishes');  --lc
-insert into ingredients values (36, 'Leberkaese', '', 'leberkaese'); --hf
-insert into ingredients values (37, 'Lagenbroetchen', '', 'roll'); --hc
+insert into ingredients values (33, 'Senf', '', 'senf'); 
+insert into ingredients values (34, 'Coleslaw', '', 'salat'); 
+insert into ingredients values (35, 'Radishes', '', 'radishes');  
+insert into ingredients values (36, 'Leberkaese', '', 'leberkaese'); 
+insert into ingredients values (37, 'Laugenbroetchen', '', 'roll'); 
 
 --Leberkaese Ersatz 
 insert into ingredients values (38, 'Gluten free roll', '', 'roll');
---- + meat replacement, soy
+-- + soy
+
+--Coclate Brownie
+insert into ingredients values (39, 'Coconut oil', '', 'oil'); 
+insert into ingredients values (40, 'Sugar', '', 'salat'); 
+insert into ingredients values (41, 'Cocoa Powder', '', 'cocoa');  
+insert into ingredients values (42, 'Banana', '', 'egg'); 
+insert into ingredients values (43, 'flour', '', 'roll'); 
+insert into ingredients values (44, 'Chocolate', '', 'choc'); 
+insert into ingredients values (44, 'Baking powder', '', 'bakpow'); 
+
+--Dumplings
+insert into ingredients values (45, 'roll', '', 'roll'); 
+insert into ingredients values (46, 'Petersilie', '', 'herbs'); 
+insert into ingredients values (47, 'Semmelbrösel', '', 'roll');
+
+
+--_______________________________________________________
+
 
 -- AMOUNT AND ADD INGREDIENTS TO RECIPE
 -- Unique, receipe id, ingridient id, amount, unit  
 --Mushroom Risotto
-insert into recipe_ingredients values (1, 1, 3, 500, 'ml');
-insert into recipe_ingredients values (2, 2, 1, 200, 'g');
-insert into recipe_ingredients values (3, 2, 2, 1.5, 'pcs');
+insert into recipe_ingredients values (1, 1, 1, 500, 'g');
+insert into recipe_ingredients values (2, 1, 2, 100, 'g');
+insert into recipe_ingredients values (3, 1, 3, 1000, 'ml');
+insert into recipe_ingredients values (4, 1, 4, 1, 'pcs');
+insert into recipe_ingredients values (5, 1, 5, 2, 'pcs');
+insert into recipe_ingredients values (6, 1, 6, 200, 'g');
+insert into recipe_ingredients values (7, 1, 7, 200, 'g');
+insert into recipe_ingredients values (8, 1, 8, 100, 'g');
+insert into recipe_ingredients values (9, 1, 9, 5, 'g');
+insert into recipe_ingredients values (10, 1, 10, 8, 'g');
+insert into recipe_ingredients values (11, 1, 11, 100, 'g');
+insert into recipe_ingredients values (12, 1, 12, 50, 'g');
+insert into recipe_ingredients values (13, 1, 13, 50, 'ml');
+insert into recipe_ingredients values (14, 1, 14, 20, 'g');
+
+insert into recipe_ingredients values (15, 3, 33, 7, 'g');
+insert into recipe_ingredients values (16, 3, 34, 50, 'g');
+insert into recipe_ingredients values (17, 3, 35, 2, 'pcs');
+insert into recipe_ingredients values (18, 3, 36, 2, 'pcs');
+insert into recipe_ingredients values (19, 3, 37, 1, 'pcs');
 
 -- FOOD GROUPS id, id ingredient, food group id (1, 'Lactose',
 -- 2, 'Gluten' 3, 'Histamines', 4, 'Seafood')
@@ -93,15 +127,16 @@ insert into ingredient_special_diets values(5, 35, 1);
 insert into ingredient_special_diets values(6, 38, 2);
 insert into ingredient_special_diets values(7, 1, 4);
 insert into ingredient_special_diets values(8, 33, 4);
-insert into ingredient_special_diets values(3, 16, 3);
+insert into ingredient_special_diets values(10, 16, 3);
 
 
 
 -- diet gols id, ingredient_id INTEGER NOT NULL, diet_goal_id INT
 -- (1, 'Vegan'),(2, 'Vegetarian'),(3, 'Pescetarian'),(4, 'Keto');
-insert into ingredient_diet_goals values(1, 4, 3);
-insert into ingredient_diet_goals values(2, 4, 3);
-insert into ingredient_diet_goals values(3, 4, 3);
-
+insert into ingredient_diet_goals values(1, 33, 1);
+insert into ingredient_diet_goals values(2, 34, 1);
+insert into ingredient_diet_goals values(3, 35, 1);
+insert into ingredient_diet_goals values(4, 36, 4);
+insert into ingredient_diet_goals values(5, 37, 2);
 
 --hated foods
