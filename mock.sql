4e698b9825333f733fe56fa72d4566fff27d8a4f
blackSwan566 <57305281+blackSwan566@users.noreply.github.com>
2023-11-18 19:11:18 +0100

    More Ingredients, food groups

--- mock.sql
+++ mock.sql
@@ -9,25 +9,41 @@ insert into ingredients values (2, 'Mushrooms', 'Normal');
 insert into ingredients values (3, 'Water', 'Tap');
 insert into ingredients values (4, 'Onions', '');
 insert into ingredients values (5, 'Garlic', '');
-insert into ingredients values (7, 'Rice', 'For risotto');
-insert into ingredients values (8, 'Rice', 'Basmati');
-insert into ingredients values (9, 'Freezed peas', '');
-insert into ingredients values (10, 'Salt', '');
-insert into ingredients values (11, 'Pepper', '');
-insert into ingredients values (12, 'Parmesan', '');
-insert into ingredients values (13, 'Butter', '');
-insert into ingredients values (14, 'Plant Oil', '');
-insert into ingredients values (15, 'Basil', '');
+insert into ingredients values (6, 'Rice', 'For risotto');
+insert into ingredients values (7, 'Rice', 'Basmati');
+insert into ingredients values (8, 'Frozen peas', '');
+insert into ingredients values (9, 'Salt', '');
+insert into ingredients values (10, 'Pepper', '');
+insert into ingredients values (11, 'Parmesan', '');
+insert into ingredients values (12, 'Butter', '');
+insert into ingredients values (13, 'Plant Oil', '');
+insert into ingredients values (14, 'Basil', '');
 
---Ingridients Mushroom Risotto Vegan
-insert into ingredients values (16, 'Vegan white wine', '');
-insert into ingredients values (17, 'Vegan butter', '');
-insert into ingredients values (18, 'Vegan Pramesan', '');
+--Ingredients mushroom risotto vegan
+insert into ingredients values (15, 'Vegan white wine', '');
+insert into ingredients values (16, 'Vegan butter', '');
+insert into ingredients values (17, 'Vegan Pramesan', '');
 
---Uniqueid, receipe id, ingridient id, anzahl,einheit 
+-- More ingredients
+insert into ingredients values (18, 'Milk', '');
+insert into ingredients values (19, 'Egg', '');
+insert into ingredients values (20, 'Shrimps', '');
+insert into ingredients values (21, 'Soy', '');
+
+-- AMOUNT AND ADD INGREDIENTS TO RECIPE
+-- Uniqueid, receipe id, ingridient id, anzahl,einheit 
+--Mushroom Risotto
 insert into recipe_ingredients values (1, 1, 3, 500, 'ml');
+
 insert into recipe_ingredients values (2, 2, 1, 200, 'g');
 insert into recipe_ingredients values (3, 2, 2, 1.5, 'pcs');
 
--- FOOD GROUPS
-insert into ingredient_food_groups values()
+-- FOOD GROUPS id, id ingredient, food group id (1, 'Lactose',
+-- 2, 'Gluten' 3, 'Histamines', 4, 'Seafood')
+insert into ingredient_food_groups values(1, 4, 3);
+insert into ingredient_food_groups values(2, 5, 3);
+insert into ingredient_food_groups values(3, 6, 3);
+insert into ingredient_food_groups values(4, 8, 3);
+insert into ingredient_food_groups values(5, 11, 3);
+insert into ingredient_food_groups values(5, 11, 1);
+insert into ingredient_food_groups values(6, 12, 1);
