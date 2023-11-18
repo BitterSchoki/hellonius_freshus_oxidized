f1c6d319df846db5cb5829bbd9be3a04161606b8
Florian Lercher <florian.lercher@tum.de>
2023-11-18 15:12:39 +0100

    Add down migration for allergies

--- migrations/20231118124846_allergies.down.sql
+++ migrations/20231118124846_allergies.down.sql
@@ -1,1 +1,6 @@
--- Add down migration script here
+DROP TABLE ingredient_special_diets;
+DROP TABLE ingredient_diet_goals;
+DROP TABLE ingredient_food_groups;
+DROP TABLE special_diets;
+DROP TABLE diet_goals;
+DROP TABLE food_groups;
